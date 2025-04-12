b46d6bf2059b2805b558c905d7f3ccf867937908
Shivang Patel <shivangpatel16382@gmail.com>
2025-04-11 21:34:35 -0400

    first commit a6

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js"; 
 import AssignmentsRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
+import mongoose from "mongoose"; 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz" 
+mongoose.connect(CONNECTION_STRING); 
 
 
 
